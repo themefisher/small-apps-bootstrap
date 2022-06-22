72c708bdf882a3ee9dc063d307998b86c997ac54
tfsomrat <themefisher.dev@gmail.com>
2022-06-22 15:41:52 +0600

    update dependencies

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Small Apps By Themefisher
-Small Apps is a clean and modern Free responsive app landing-page template for Mobile App. Built with Bootstrap 4.x frontend Framework. The codebase is well organized, very easy to customize, and SEO optimized.
 
+Small Apps is a clean and modern Free responsive app landing-page template for Mobile App. Built with Bootstrap 4.x frontend Framework. The codebase is well organized, very easy to customize, and SEO optimized.
 
 <!-- demo -->
 ## Example Site
+
 | [![](screenshots/homepage.png)](https://demo.themefisher.com/small-apps/) | [![](screenshots/homepage-2.png)](http://localhost:3000/homepage-2.html) | [![](screenshots/homepage-3.png)](https://demo.themefisher.com/small-apps/homepage-3.html) |
 |:---:|:---:|:---:|
 | **Homepage 1**  | **Homepage 2**  | **Homepage 3**  |
@@ -16,6 +17,7 @@ Small Apps is a clean and modern Free responsive app landing-page template for M
 
 <!-- resources -->
 ## Pages
+
 * **Homepage 1**
 * **Homepage 2**
 * **Homepage 3**
@@ -29,75 +31,94 @@ Small Apps is a clean and modern Free responsive app landing-page template for M
 * **Coming Soon**
 * **404**
 * **Privacy Policy**
-* **Signin**
+* **Sign in**
 * **Signup**
-
 
 <!-- download -->
 ## Download And installation
-Download this template from any following options:
 
-* Download from [Github](https://github.com/themefisher/Small-Apps-Bootstrap-App-Landing-Template/archive/master.zip)
-* Clone the repository: `git clone https://github.com/themefisher/Small-Apps-Bootstrap-App-Landing-Template.git`
-* Download from [Themefisher](https://themefisher.com/products/small-apps-free-app-landing-page-template/)
-
+Download this template from [Github](https://github.com/themefisher/small-apps/archive/main.zip)
 
 <!-- installation -->
-### Basic Usage
-After downloading template, you can simply edit the HTML and CSS files from the `theme` folder. To preview the changes you make to the code, you can open the index.html file in your web browser.
+### Basic Usages
+
+After downloading the template, you can simply edit the HTML and CSS files from the `theme` folder. To preview the changes you make to the code, you can open the index.html file in your web browser.
 
 ### Advanced Usage
-For advanced usage you have some dependencies to install. Then you can run it on your localhost. You can view the package.json file to see which scripts are included.
 
-#### Install Dependencies
+For advanced usage, you have some prerequisites to install. Then you can run it on your localhost. You can view the package.json file to see which scripts are included.
+
+#### Install prerequisites (once for a machine)
+
 * **Node Installation:** [Install node js](https://nodejs.org/en/download/)
-* **Gulp Installation:** Install gulp globally from your terminal 
+* **Gulp Installation:** Install gulp globally from your terminal
+
 ```
 npm install --global gulp-cli
 ```
-Or visit original [docs](https://gulpjs.com/docs/en/getting-started/quick-start)
 
-#### Run Theme
-After succesfully install those dependencies, open this theme with any IDE [[VS Code](https://code.visualstudio.com/) recommended], and then open internal terminal of IDE [vs code shortcut <code>ctrl/cmd+\`</code>]
+Or visit the original [Gulp docs](https://gulpjs.com/docs/en/getting-started/quick-start)
 
-* Install node package modules
+#### Local setup
+
+After successfully installing those dependencies, open this theme with any IDE [[VS Code](https://code.visualstudio.com/) recommended], and then open the internal terminal of IDM [vs code shortcut <code>ctrl/cmd+\`</code>]
+
+* Install dependencies
+
 ```
 npm install
 ```
-* Run gulp
-```
-gulp
-```
-After that, it will open up a preview of the template in your default browser, watch for changes to core template files, and live reload the browser when changes are saved.
 
-👉🏻 [visit documentation](https://docs.themefisher.com/airspace/)
+* Run locally
 
+```
+npm run dev
+```
+
+After that, it will open up a preview of the template in your default browser, watch for changes to source files, and live reload the browser when changes are saved.
+
+#### Production Build
+
+After finishing all the customization, you can create a production build by running this command.
+
+```
+npm run build
+```
+
+Now you get a `theme` folder that has all the changes you have made. you can use this folder as your main theme.
+
+👉🏻 [visit documentation](https://docs.themefisher.com/small-apps/)
 
 <!-- reporting issue -->
 ## Reporting Issues
-We use GitHub Issues as the official bug tracker for the Small Apps Template. Please Search [existing issues](https://github.com/themefisher/Small-Apps-Bootstrap-App-Landing-Template/issues). It’s possible someone has already reported the same problem.
-If your problem or idea has not been addressed yet, feel free to [open a new issue](https://github.com/themefisher/Small-Apps-Bootstrap-App-Landing-Template/issues).
+
+We use GitHub Issues as the official bug tracker for the Small Apps Template. Please Search [existing issues](https://github.com/themefisher/small-apps/issues). Someone may have already reported the same problem.
+If your problem or idea has not been addressed yet, feel free to [open a new issue](https://github.com/themefisher/small-apps/issues).
 
 <!-- support -->
 ## Technical Support or Questions (Paid)
+
 If you have questions or need help integrating the product please [contact us](mailto:mehedi@themefisher.com) instead of opening an issue.
 
 <!-- licence -->
 ## License
-Copyright &copy; 2021 Designed & Developed by [Themefisher](https://themefisher.com)
 
-**Code License:** Released under the [MIT](https://github.com/themefisher/Small-Apps-Bootstrap-App-Landing-Template/blob/master/LICENSE) license.
+Copyright (c) 2016 - Present, Designed & Developed by [Themefisher](https://themefisher.com)
 
-**Image license:** The images are only for demonstration purposes. They have their own licence, we don't have permission to share those image.
+**Code License:** Released under the [MIT](https://github.com/themefisher/small-apps/blob/main/LICENSE) license.
+
+**Image license:** The images are only for demonstration purposes. They have their license, we don't have permission to share those images.
 
 <!-- resources -->
 ## Resources
-Some third-party plugins that we used to build this template. Please check their licence.
-* **Bootstrap v4.5**: https://getbootstrap.com/docs/4.5/getting-started/introduction/
-* **Jquery v3.5.1**: https://jquery.com/download/
-* **Themify Icons**: https://themify.me/themify-icons
-* **Google Fonts**: http://fonts.google.com/
-* **AOS**: https://michalsnik.github.io/aos/
-* **Fancybox**: http://fancyapps.com/fancybox/
-* **Slick**: https://kenwheeler.github.io/slick/
-* **SyoTimer**: http://syomochkin.xyz/folio/syotimer/demo.html
+
+Some third-party plugins that we used to build this template. Please check their license.
+
+* **Bootstrap v4.5**: <https://getbootstrap.com/docs/4.5/getting-started/introduction/>
+* **Jquery v3.5.1**: <https://jquery.com/download/>
+* **Themify Icons**: <https://themify.me/themify-icons>
+* **Google Fonts**: <http://fonts.google.com/>
+* **AOS**: <https://michalsnik.github.io/aos/>
+* **Fancybox**: <http://fancyapps.com/fancybox/>
+* **Slick**: <https://kenwheeler.github.io/slick/>
+* **SyoTimer**: <http://syomochkin.xyz/folio/syotimer/demo.html>
--- gulpfile.js
+++ gulpfile.js
@@ -1,199 +1,177 @@
-'use strict';
+"use strict";
 
-const sass = require('gulp-sass')(require('sass'));
-const gulp = require('gulp');
-const sourcemaps = require('gulp-sourcemaps');
-const fileinclude = require('gulp-file-include');
-const autoprefixer = require('gulp-autoprefixer');
-const bs = require('browser-sync').create();
-const rimraf = require('rimraf');
-const comments = require('gulp-header-comment');
+const sass = require("gulp-sass")(require("sass"));
+const gulp = require("gulp");
+const sourcemaps = require("gulp-sourcemaps");
+const fileinclude = require("gulp-file-include");
+const autoprefixer = require("gulp-autoprefixer");
+const bs = require("browser-sync").create();
+const rimraf = require("rimraf");
+const comments = require("gulp-header-comment");
 
 var path = {
   src: {
-    html: 'source/*.html',
-    others: 'source/*.+(php|ico|png)',
-    htminc: 'source/partials/**/*.htm',
-    incdir: 'source/partials/',
-    plugins: 'source/plugins/**/*.*',
-    js: 'source/js/*.js',
-    scss: 'source/scss/**/*.scss',
-    images: 'source/images/**/*.+(png|jpg|gif|svg)'
+    html: "source/*.html",
+    others: "source/*.+(php|ico|png)",
+    htminc: "source/partials/**/*.htm",
+    incdir: "source/partials/",
+    plugins: "source/plugins/**/*.*",
+    js: "source/js/*.js",
+    scss: "source/scss/**/*.scss",
+    images: "source/images/**/*.+(png|jpg|gif|svg)",
   },
   build: {
-    dirNetlify: 'netlify/',
-    dirDev: 'theme/'
-  }
+    dirBuild: "theme/",
+    dirDev: "theme/",
+  },
 };
 
 // HTML
-gulp.task('html:build', function () {
-  return gulp.src(path.src.html)
-    .pipe(fileinclude({
-      basepath: path.src.incdir
-    }))
-    .pipe(comments(`
+gulp.task("html:build", function () {
+  return gulp
+    .src(path.src.html)
+    .pipe(
+      fileinclude({
+        basepath: path.src.incdir,
+      })
+    )
+    .pipe(
+      comments(`
     WEBSITE: https://themefisher.com
     TWITTER: https://twitter.com/themefisher
     FACEBOOK: https://www.facebook.com/themefisher
     GITHUB: https://github.com/themefisher/
-    `))
+    `)
+    )
     .pipe(gulp.dest(path.build.dirDev))
-    .pipe(bs.reload({
-      stream: true
-    }));
+    .pipe(
+      bs.reload({
+        stream: true,
+      })
+    );
 });
 
 // SCSS
-gulp.task('scss:build', function () {
-  return gulp.src(path.src.scss)
+gulp.task("scss:build", function () {
+  return gulp
+    .src(path.src.scss)
     .pipe(sourcemaps.init())
-    .pipe(sass({
-      outputStyle: 'expanded'
-    }).on('error', sass.logError))
+    .pipe(
+      sass({
+        outputStyle: "expanded",
+      }).on("error", sass.logError)
+    )
     .pipe(autoprefixer())
-    .pipe(sourcemaps.write('/'))
-    .pipe(comments(`
+    .pipe(sourcemaps.write("/"))
+    .pipe(
+      comments(`
     WEBSITE: https://themefisher.com
     TWITTER: https://twitter.com/themefisher
     FACEBOOK: https://www.facebook.com/themefisher
     GITHUB: https://github.com/themefisher/
-    `))
-    .pipe(gulp.dest(path.build.dirDev + 'css/'))
-    .pipe(bs.reload({
-      stream: true
-    }));
+    `)
+    )
+    .pipe(gulp.dest(path.build.dirDev + "css/"))
+    .pipe(
+      bs.reload({
+        stream: true,
+      })
+    );
 });
 
 // Javascript
-gulp.task('js:build', function () {
-  return gulp.src(path.src.js)
-    .pipe(comments(`
+gulp.task("js:build", function () {
+  return gulp
+    .src(path.src.js)
+    .pipe(
+      comments(`
   WEBSITE: https://themefisher.com
   TWITTER: https://twitter.com/themefisher
   FACEBOOK: https://www.facebook.com/themefisher
   GITHUB: https://github.com/themefisher/
-  `))
-    .pipe(gulp.dest(path.build.dirDev + 'js/'))
-    .pipe(bs.reload({
-      stream: true
-    }));
+  `)
+    )
+    .pipe(gulp.dest(path.build.dirDev + "js/"))
+    .pipe(
+      bs.reload({
+        stream: true,
+      })
+    );
 });
 
 // Images
-gulp.task('images:build', function () {
-  return gulp.src(path.src.images)
-    .pipe(gulp.dest(path.build.dirDev + 'images/'))
-    .pipe(bs.reload({
-      stream: true
-    }));
+gulp.task("images:build", function () {
+  return gulp
+    .src(path.src.images)
+    .pipe(gulp.dest(path.build.dirDev + "images/"))
+    .pipe(
+      bs.reload({
+        stream: true,
+      })
+    );
 });
 
 // Plugins
-gulp.task('plugins:build', function () {
-  return gulp.src(path.src.plugins)
-    .pipe(gulp.dest(path.build.dirDev + 'plugins/'))
-    .pipe(bs.reload({
-      stream: true
-    }));
+gulp.task("plugins:build", function () {
+  return gulp
+    .src(path.src.plugins)
+    .pipe(gulp.dest(path.build.dirDev + "plugins/"))
+    .pipe(
+      bs.reload({
+        stream: true,
+      })
+    );
 });
 
 // Other files like favicon, php, sourcele-icon on root directory
-gulp.task('others:build', function () {
-  return gulp.src(path.src.others)
-    .pipe(gulp.dest(path.build.dirDev))
+gulp.task("others:build", function () {
+  return gulp.src(path.src.others).pipe(gulp.dest(path.build.dirDev));
 });
 
 // Clean Build Folder
-gulp.task('clean', function (cb) {
-  rimraf('./theme', cb);
+gulp.task("clean", function (cb) {
+  rimraf("./theme", cb);
 });
 
 // Watch Task
-gulp.task('watch:build', function () {
-  gulp.watch(path.src.html, gulp.series('html:build'));
-  gulp.watch(path.src.htminc, gulp.series('html:build'));
-  gulp.watch(path.src.scss, gulp.series('scss:build'));
-  gulp.watch(path.src.js, gulp.series('js:build'));
-  gulp.watch(path.src.images, gulp.series('images:build'));
-  gulp.watch(path.src.plugins, gulp.series('plugins:build'));
+gulp.task("watch:build", function () {
+  gulp.watch(path.src.html, gulp.series("html:build"));
+  gulp.watch(path.src.htminc, gulp.series("html:build"));
+  gulp.watch(path.src.scss, gulp.series("scss:build"));
+  gulp.watch(path.src.js, gulp.series("js:build"));
+  gulp.watch(path.src.images, gulp.series("images:build"));
+  gulp.watch(path.src.plugins, gulp.series("plugins:build"));
 });
 
-// Build Task
-gulp.task('default', gulp.series(
-  'clean',
-  'html:build',
-  'js:build',
-  'scss:build',
-  'images:build',
-  'plugins:build',
-  'others:build',
-  gulp.parallel(
-    'watch:build',
-    function () {
+// Dev Task
+gulp.task(
+  "default",
+  gulp.series(
+    "clean",
+    "html:build",
+    "js:build",
+    "scss:build",
+    "images:build",
+    "plugins:build",
+    "others:build",
+    gulp.parallel("watch:build", function () {
       bs.init({
         server: {
           baseDir: path.build.dirDev,
-        }
+        },
       });
     })
   )
 );
 
-
-/* =====================================================
-Netlify Builds
-===================================================== */
-// HTML
-gulp.task('html:netlify:build', function () {
-  return gulp.src(path.src.html)
-    .pipe(fileinclude({
-      basepath: path.src.incdir
-    }))
-    .pipe(gulp.dest(path.build.dirNetlify));
-});
-
-// SCSS
-gulp.task('scss:netlify:build', function () {
-  return gulp.src(path.src.scss)
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-      outputStyle: 'expanded'
-    }).on('error', sass.logError))
-    .pipe(autoprefixer())
-    .pipe(sourcemaps.write('/maps'))
-    .pipe(gulp.dest(path.build.dirNetlify + 'css/'));
-});
-
-// Javascript
-gulp.task('js:netlify:build', function () {
-  return gulp.src(path.src.js)
-    .pipe(gulp.dest(path.build.dirNetlify + 'js/'));
-});
-
-// Images
-gulp.task('images:netlify:build', function () {
-  return gulp.src(path.src.images)
-    .pipe(gulp.dest(path.build.dirNetlify + 'images/'));
-});
-
-// Plugins
-gulp.task('plugins:netlify:build', function () {
-  return gulp.src(path.src.plugins)
-    .pipe(gulp.dest(path.build.dirNetlify + 'plugins/'))
-});
-
-// Other files like favicon, php, apple-icon on root directory
-gulp.task('others:netlify:build', function () {
-  return gulp.src(path.src.others)
-    .pipe(gulp.dest(path.build.dirNetlify))
-});
-
 // Build Task
-gulp.task('netlify', gulp.series(
-  'html:netlify:build',
-  'js:netlify:build',
-  'scss:netlify:build',
-  'images:netlify:build',
-  'plugins:netlify:build'
-));
+gulp.task(
+  "build",
+  gulp.series(
+    "html:build",
+    "js:build",
+    "scss:build",
+    "images:build",
+    "plugins:build"
+  )
+);
